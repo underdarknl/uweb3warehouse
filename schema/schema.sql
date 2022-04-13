a78b710aceb56a9d6d752afbfe0da617d2a69b4b
Jan <jan@underdark.nl>
2022-04-13 16:24:29 +0200

    update schema to include all relevant tables

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,5 +1,7 @@
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `apiuser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apiuser` (
   `ID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `key` char(32) NOT NULL,
@@ -10,8 +12,104 @@ CREATE TABLE `apiuser` (
   UNIQUE KEY `name` (`name`),
   KEY `active` (`active`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `ID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `ean` char(13) DEFAULT NULL,
+  `gs1` smallint(5) unsigned DEFAULT NULL,
+  `description` text,
+  `supplier` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `cost` decimal(6,3) DEFAULT NULL,
+  `assemblycosts` decimal(5,3) NOT NULL DEFAULT '0.000',
+  `vat` decimal(4,2) NOT NULL DEFAULT '0.00',
+  `sku` varchar(45) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateDeleted` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `name_UNIQUE` (`name`,`dateDeleted`),
+  UNIQUE KEY `gs1_UNIQUE` (`gs1`,`dateDeleted`),
+  UNIQUE KEY `sku_UNIQUE` (`supplier`,`sku`,`dateDeleted`),
+  KEY `supplier` (`supplier`),
+  CONSTRAINT `supplier` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `productpart`
+--
+
+DROP TABLE IF EXISTS `productpart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productpart` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product` mediumint(8) unsigned NOT NULL,
+  `part` mediumint(8) unsigned DEFAULT NULL,
+  `amount` smallint(5) unsigned NOT NULL,
+  `assemblycosts` decimal(5,3) NOT NULL DEFAULT '0.000',
+  PRIMARY KEY (`ID`),
+  KEY `product` (`product`),
+  KEY `part` (`part`),
+  CONSTRAINT `part` FOREIGN KEY (`part`) REFERENCES `product` (`ID`) ON UPDATE CASCADE,
+  CONSTRAINT `product` FOREIGN KEY (`product`) REFERENCES `product` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock` (
+  `ID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `product` mediumint(8) unsigned NOT NULL,
+  `amount` mediumint(9) NOT NULL,
+  `reference` varchar(45) DEFAULT NULL,
+  `lot` varchar(45) DEFAULT NULL,
+  `dateCreated` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supplier` (
+  `ID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `telephone` varchar(45) DEFAULT NULL,
+  `contact_person` varchar(255) DEFAULT NULL,
+  `email_address` varchar(255) DEFAULT NULL,
+  `gscode` varchar(15) DEFAULT NULL,
+  `dateDeleted` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `name_UNIQUE` (`name`,`dateDeleted`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `ID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
@@ -22,3 +120,13 @@ CREATE TABLE `user` (
   KEY `login` (`email`,`password`,`active`),
   KEY `active` (`active`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
