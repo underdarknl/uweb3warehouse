c275089bd6269e9dbd02fa930c16abb9e9fc7c18
stefvanhouten <stefvanhouten@gmail.com>
2022-05-28 10:36:26 +0200

    Add StockImporter that can parse suppliers email. This attempts to find the corresponding warehouse product.

---
+++ base/libs/stock_importer.py
@@ -0,0 +1,81 @@
+import pandas
+
+from base import model
+
+
+class StockParser:
+    def __init__(self, file_path, columns, normalize_columns):
+        self.columns = columns
+        self.normalize_columns = normalize_columns
+        self.file_path = file_path
+
+    def Parse(self):
+        results = self._parse()
+        return self._matches(results)
+
+    def _parse(self):
+        return pandas.read_html(self.file_path, header=0, parse_dates=True)
+
+    def _matches(self, results):
+        processed = []
+        for matches in results:
+            processed.append(self._process_matches(matches.to_dict("record")))
+        return processed
+
+    def _process_matches(self, matches):
+        for match in matches:
+            self._normalize(match)
+        return matches
+
+    def _normalize(self, match):
+        for column in self.normalize_columns:
+            match[column] = self._normalize_column_value(match[column])
+
+    def _normalize_column_value(self, value):
+        match = " ".join(value.split())
+        values = [match]
+
+        if "-" in match:
+            match = match.replace(" ", "_")
+            match = match.replace("-", "_")
+            values.append(match)
+        if " " in match:
+            values.append(match.replace(" ", "_"))
+        # if "(" in match:
+        #     values.append(match.split("(")[0])
+        return values
+
+
+class StockImporter:
+    def __init__(self, connection, results):
+        self.results = results
+        self.connection = connection
+        self.products = None
+
+    def Import(self):
+        self.load_product_data()
+        for product_list in self.results:
+            self._import_list(product_list)
+
+    def load_product_data(self):
+        self.products = list(model.Product.List(self.connection))
+
+    def _import_list(self, products):
+        for product in products:
+            self._import(product)
+
+    def _import(self, result):
+        for name in result["Type"]:
+            for product in self.products:
+                if name == product["name"]:
+                    self._add_stock(product, result["Op voorraad"])
+                    break
+
+    def _add_stock(self, product, amount):
+        return model.Stock.Create(
+            self.connection,
+            {
+                "product": product.key,
+                "amount": amount,
+            },
+        )
