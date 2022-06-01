5f4ce1ae11bf2da0bdc66cea4a8c22a499950c3b
stefvanhouten <stefvanhouten@gmail.com>
2022-06-01 15:29:13 +0200

    Add wtforms

---
+++ warehouse/products/forms.py
@@ -0,0 +1,21 @@
+from wtforms import (
+    DecimalField,
+    Form,
+    IntegerField,
+    StringField,
+    TextAreaField,
+    validators,
+)
+
+
+class ProductForm(Form):
+    sku = StringField("sku", [validators.Length(min=1, max=45)])
+    name = StringField(
+        "name", [validators.Length(min=5, max=255), validators.DataRequired()]
+    )
+    ean = StringField("ean", [validators.Length(min=1, max=13), validators.Optional()])
+    gs1 = IntegerField(
+        "gs1", [validators.NumberRange(min=1, max=32767), validators.Optional()]
+    )
+    description = TextAreaField("description", validators=[validators.Optional()])
+    assemblycosts = DecimalField("assemblycosts", validators=[validators.Optional()])
