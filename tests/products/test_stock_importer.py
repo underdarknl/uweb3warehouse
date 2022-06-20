f13d3ef992a1c64d5d75ee757225ad75aa2a6b0a
stefvanhouten <stefvanhouten@gmail.com>
2022-06-20 11:16:14 +0200

    Update test_stock_importer.py

--- tests/products/test_stock_importer.py
+++ tests/products/test_stock_importer.py
@@ -5,20 +5,75 @@ from warehouse.products.helpers import StockImporter
 
 
 class TestStockImporter:
-    def test_parse(self, products):
+    @pytest.mark.parametrize(
+        "data, expected",
+        [
+            (
+                [[{"Op voorraad": 20, "Product": "Product 1"}]],
+                {"name": "Product 1", "sku": "sku-1", "amount": 20},
+            ),
+            (
+                [[{"Op voorraad": 100, "Product": "Product 2"}]],
+                {"name": "Product 2", "sku": "sku-2", "amount": 100},
+            ),
+            (
+                [[{"Op voorraad": 5, "Product": "Product 3"}]],
+                {"name": "Product 3", "sku": "sku-3", "amount": 5},
+            ),
+        ],
+    )
+    def test_parse(self, products, data, expected):
         importer = StockImporter(
             {
                 "amount": "Op voorraad",
                 "name": "Product",
             }
         )
-        data = [[{"Op voorraad": 20, "Product": "Product 1"}]]
         processed, unprocessed = importer.Import(data, products)
 
         assert len(processed) == 1
         assert len(unprocessed) == 0
 
-        assert processed[0].parsed_product["name"] == "Product 1"
+        assert expected["name"] == processed[0].parsed_product["name"]
+
         # Make sure it matched the correct supplier product
-        assert processed[0].supplier_product["sku"] == "sku-1"
-        assert processed[0].parsed_product["amount"] == 20
+        assert expected["sku"] == processed[0].supplier_product["sku"]
+        assert expected["amount"] == processed[0].parsed_product["amount"]
+
+    @pytest.mark.parametrize(
+        "data, expected",
+        [
+            (
+                [[{"Op voorraad": 20, "Product": "Product 12"}]],
+                [{"name": "Product 12", "amount": 20}],
+            ),
+            (
+                [
+                    [{"Op voorraad": 20, "Product": "Product 1"}],
+                    [{"Op voorraad": 10, "Product": "Product 12"}],
+                    [{"Op voorraad": 5, "Product": "Pro"}],
+                ],
+                [{"name": "Product 12", "amount": 10}, {"name": "Pro", "amount": 5}],
+            ),
+        ],
+    )
+    def test_parse_no_result(self, products, data, expected):
+        importer = StockImporter(
+            {
+                "amount": "Op voorraad",
+                "name": "Product",
+            }
+        )
+        _, unprocessed = importer.Import(data, products)
+        assert unprocessed == expected
+
+    def test_wrong_mapping(self, products):
+        importer = StockImporter(
+            {
+                "amount": "Op voorraad",
+                "name": "Product",
+            }
+        )
+        data = [{"wrong_key": 20, "Product": "Product 1"}]
+        with pytest.raises(TypeError):
+            importer.Import(data, products)
