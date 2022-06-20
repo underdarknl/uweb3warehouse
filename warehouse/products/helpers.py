9886538b9720e439761741ffcdee15410aacf67d
stefvanhouten <stefvanhouten@gmail.com>
2022-06-20 10:26:46 +0200

    Clean up parser and fix issue where no error was raised when a column was only a partial match.

--- warehouse/products/helpers.py
+++ warehouse/products/helpers.py
@@ -22,7 +22,7 @@ class StockParser:
         Args:
             file_path (StringIO): The StringIO object containing the HTML with the table.
             columns (tuple[str]): The columns that we are interested in
-            normalize_columns (_type_): The column containing the product name that should be normalized to match the database value.
+            normalize_columns (tuple[str]): The column containing the product name that should be normalized to match the database value.
         """
         self.columns = columns
         self.normalize_columns = normalize_columns
@@ -46,29 +46,49 @@ class StockParser:
         Returns:
             list[dict]: Returns the list with the matches.
         """
-        processed = []
         # Because multiple tables can be present in a page we can have multiple dataframes.
-        for dataframe in dataframes:
-            processed.append(self._process_dataframe(dataframe.to_dict("record")))
-        return processed
+        return [
+            self._process_dataframe(dataframe.to_dict("record"))
+            for dataframe in dataframes
+        ]
 
     def _process_dataframe(self, dataframe):
-        """Mutate the dataframe by normalizing the columns that we are interested in so that the value matches the value stored in the database."""
+        """Process the dataframe by normalizing the values contained in the columns.
+
+        Returns:
+            list[dict]: A list of dictionaries with the processed matches.
+        """
+        results = []
+
         for result in dataframe:
-            missing_columns = [
-                column for column in self.columns if column not in result
-            ]
-            if missing_columns:
+            if any(
+                missing_columns := [
+                    column for column in self.columns if column not in result.keys()
+                ]
+            ):
                 raise KeyError(
                     f"The following columns could not be found: {missing_columns}"
                 )
-            self._normalize(result)
-        return dataframe
 
-    def _normalize(self, match):
+            results.append(self._normalize(result))
+
+        return results
+
+    def _normalize(self, result):
         """Normalize only the columns which are of interest. This should be the column containing the product name."""
+        clean_result = self._remove_unwanted_keys(result)
+
         for column in self.normalize_columns:
-            match[column] = match[column].replace("/", "_")
+            clean_result[column] = clean_result[column].replace("/", "_")
+        return clean_result
+
+    def _remove_unwanted_keys(self, result):
+        """Create a copy of the result and mutate the object by removing keys that are not sought after."""
+        copy = dict(result)
+        for key in result.keys():
+            if key not in self.columns:
+                del copy[key]
+        return copy
 
 
 class StockImporter:
