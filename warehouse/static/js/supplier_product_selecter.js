ce80b7cc588bb5a69d1fbe26da40da34ab28ac5e
stefvanhouten <stefvanhouten@gmail.com>
2022-07-01 10:15:12 +0200

    Populate hidden fields with clean product name and sku if available.

--- warehouse/static/js/supplier_product_selecter.js
+++ warehouse/static/js/supplier_product_selecter.js
@@ -9,10 +9,12 @@
             this.supplierProducts = [];
             this.selectedSupplier =
                 document.getElementById("selected_supplier");
-            this.inputField = document.getElementById("supplier_product_input");
+            this.inputField = document.getElementById("supplier_product");
             this.datalist = document.getElementById(
                 "supplier_products_datalist"
             );
+            this.hiddenSupplierProduct = document.getElementById("sup_product");
+            this.hiddenSupplierProductSku = document.getElementById("sup_sku");
 
             this.inputField.addEventListener("input", (e) => {
                 clearTimeout(this.timer);
@@ -28,15 +30,35 @@
         populate() {
             fetch(
                 `/api/v1/supplier/findproduct?supplier=${this.selectedSupplier.value}&name=${this.inputField.value}`
-            )
-                .then((response) => response.json())
+                )
+                .then((response) => {
+                    return response.json()})
                 .then((data) => {
-                    this.supplierProducts = data;
-                    this.updateList();
+                    if(data instanceof Array){
+                        this.supplierProducts = data;
+                        this.updateList();
+                    }
                 })
                 .catch((error) => console.log(error));
-        }
+                this.updateHiddenFields();
+            }
 
+        updateHiddenFields() {
+            let selected_option = Array.from(this.datalist.childNodes).find(
+                (element) => element.value == this.inputField.value
+            );
+            if (selected_option) {
+                this.hiddenSupplierProduct.value =
+                    selected_option.getAttribute("product_name");
+
+                this.hiddenSupplierProductSku.value =
+                    selected_option.getAttribute("product_sku");
+            }else{
+                this.hiddenSupplierProduct.value = "";
+
+                this.hiddenSupplierProductSku.value = "";
+            }
+        }
         updateList() {
             // remove current children
             while (this.datalist.firstChild) {
@@ -44,14 +66,19 @@
             }
             this.supplierProducts.forEach((element) => {
                 let option = document.createElement("option");
-                option.value = element?.name;
+                if (element?.supplier_sku){
+                    option.value = `[${element?.supplier_sku}] ${element?.name}`;
+                    option.setAttribute("product_sku", element?.supplier_sku);
+                }else{
+                    option.value = element?.name;
+                }
+                option.setAttribute("product_name", element?.name);
+
                 this.datalist.appendChild(option);
             });
 
             this.inputField.setAttribute("list", "supplier_products_datalist");
         }
-
-        
     }
     new InputHandler();
 })();
